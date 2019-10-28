0a1a680e6387623b12db5b318762ae548b5abd4d
tanaka_kei <tk1209adgjmptw@gmail.com>
2019-10-28 22:22:48 +0900

    blog公開用

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  data =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.data.length >= 50) {
+        event.target.disabled = true;
+      }else{
+        this.getData();
+      }
+    }, 500);
+  }
+
+  getData(){
+    let count = this.data.length +1;
+    for(let i = count;i<count+15;i++){
+      this.data.push(i);
+    }
+  }
 }
